cd85490365314ca441b25de8307dfb38ad20964a
ariel <ariel.peralta@eynes.com.ar>
2022-04-30 21:20:56 -0300

    ✏ ✂️Add crud methods

---
+++ app/crud.py
@@ -0,0 +1,58 @@
+from sqlalchemy.orm import Session
+
+from . import models, schemas
+
+
+def get_lead(db: Session, user_id: int):
+    return db.query(models.Lead).filter(models.Lead.id == user_id).first()
+
+
+def get_lead_by_email(db: Session, email: str):
+    return db.query(models.Lead).filter(models.Lead.email == email).first()
+
+
+def get_leads(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Lead).offset(skip).limit(limit).all()
+
+
+def create_bachelors_degree(db: Session, bachelors_degree: schemas.BachelorsDegreeCreate):
+    db_bachelors_degree = models.BachelorsDegree(name=bachelors_degree.name)
+    db.add(db_bachelors_degree)
+    db.commit()
+    db.refresh(db_bachelors_degree)
+    return db_bachelors_degree
+
+
+def create_signature(db: Session, signature: schemas.SignatureCreate):
+    db_signature = models.BachelorsDegree(name=signature.name,
+                                          duration=signature.duration)
+    db.add(db_signature)
+    db.commit()
+    db.refresh(db_signature)
+    return db_signature
+
+
+def create_lead(db: Session, lead: schemas.LeadCreate):
+    db_lead = models.Lead(**lead.dict())
+    db.add(db_lead)
+    db.commit()
+    db.refresh(db_lead)
+    return db_lead
+
+
+def create_projections_by_degree(db: Session, pbd: schemas.ProjectionByDegreeCreate):
+    # projections_by_degree as pbd
+    db_pbd = models.ProjectionByDegree(lead=pbd.lead)
+    db.add(db_pbd)
+    db.commit()
+    db.refresh(db_pbd)
+    return db_pbd
+
+
+def create_projections_by_signature(db: Session, pbs: schemas.ProjectionBySignatureCreate):
+    # projections_by_signature as pbs
+    db_pbs = models.ProjectionBySignature(times=pbs.times)
+    db.add(db_pbs)
+    db.commit()
+    db.refresh(db_pbs)
+    return db_pbs
