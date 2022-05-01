2498d382daeda716289cac9117fe3ecb98dc42c3
ariel <ariel.peralta@eynes.com.ar>
2022-05-01 03:06:37 -0300

    🕶️ create post methods for any model

--- app/main.py
+++ app/main.py
@@ -20,6 +20,7 @@ def get_db():
         db.close()
 
 
+# Leads Section
 @app.post("/leads/", response_model=schemas.Lead)
 def create_lead(lead: schemas.LeadCreate, db: Session = Depends(get_db)):
     db_lead = crud.get_lead_by_email(db, email=lead.email)
@@ -40,3 +41,33 @@ def read_user(lead_id: int, db: Session = Depends(get_db)):
     if db_lead is None:
         raise HTTPException(status_code=404, detail="Lead not found")
     return db_lead
+
+
+# Degree Section
+@app.post("/degree/", response_model=schemas.BachelorsDegree)
+def create_degree(degree: schemas.BachelorsDegreeCreate, db: Session = Depends(get_db)):
+    db_degree = crud.get_bachelors_degree_by_name(db, bachelors_degree_name=degree.name)
+    if db_degree:
+        raise HTTPException(status_code=400, detail="Bachelor's Degree already exist")
+    return crud.create_bachelors_degree(db=db, bachelors_degree=degree)
+
+
+# Signature Section
+@app.post("/signature/", response_model=schemas.Signature)
+def create_signature(signature: schemas.SignatureCreate, db: Session = Depends(get_db)):
+    db_signature = crud.get_signature_by_name(db, signature_name=signature.name)
+    if db_signature:
+        raise HTTPException(status_code=400, detail="Signature already exist")
+    return crud.create_signature(db=db, signature=signature)
+
+
+# Projection By Degree Section
+@app.post("/pbd/", response_model=schemas.ProjectionByDegree)
+def create_pbd(pbd: schemas.ProjectionByDegreeCreate, db: Session = Depends(get_db)):
+    return crud.create_projections_by_degree(db=db, pbd=pbd)
+
+
+# Projection By Signature Section
+@app.post("/pbs/", response_model=schemas.ProjectionBySignature)
+def create_pbs(pbs: schemas.ProjectionBySignatureCreate, db: Session = Depends(get_db)):
+    return crud.create_projections_by_signature(db=db, pbs=pbs)
