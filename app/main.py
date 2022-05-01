b006f4b0df85f902baf5f9b9855798284414749a
ariel <ariel.peralta@eynes.com.ar>
2022-05-01 08:39:50 -0300

    ✅ add dni verification and method to calculate study spend time

--- app/main.py
+++ app/main.py
@@ -23,20 +23,21 @@ def get_db():
 # Leads Section
 @app.post("/leads/", response_model=schemas.Lead)
 def create_lead(lead: schemas.LeadCreate, db: Session = Depends(get_db)):
-    db_lead = crud.get_lead_by_email(db, email=lead.email)
-    if db_lead:
+    if crud.get_lead_by_email(db, email=lead.email):
         raise HTTPException(status_code=400, detail="Email already registered")
+    if crud.get_lead_by_dni(db, dni=lead.dni):
+        raise HTTPException(status_code=400, detail="DNI already registered")
     return crud.create_lead(db=db, lead=lead)
 
 
 @app.get("/leads/", response_model=List[schemas.Lead])
-def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+def read_leads(skip: int = 0, limit: int = 25, db: Session = Depends(get_db)):
     leads = crud.get_leads(db, skip=skip, limit=limit)
     return leads
 
 
 @app.get("/leads/{lead_id}", response_model=schemas.Lead)
-def read_user(lead_id: int, db: Session = Depends(get_db)):
+def read_lead(lead_id: int, db: Session = Depends(get_db)):
     db_lead = crud.get_lead(db, lead_id=lead_id)
     if db_lead is None:
         raise HTTPException(status_code=404, detail="Lead not found")
@@ -58,6 +59,7 @@ def create_signature(signature: schemas.SignatureCreate, db: Session = Depends(g
     db_signature = crud.get_signature_by_name(db, signature_name=signature.name)
     if db_signature:
         raise HTTPException(status_code=400, detail="Signature already exist")
+    signature.time_signature = signature.monthly_duration * 4 * signature.weekly_hours
     return crud.create_signature(db=db, signature=signature)
 
 
@@ -71,3 +73,18 @@ def create_pbd(pbd: schemas.ProjectionByDegreeCreate, db: Session = Depends(get_
 @app.post("/pbs/", response_model=schemas.ProjectionBySignature)
 def create_pbs(pbs: schemas.ProjectionBySignatureCreate, db: Session = Depends(get_db)):
     return crud.create_projections_by_signature(db=db, pbs=pbs)
+
+
+@app.put("/calculate-time/{lead_id}", response_model=schemas.Lead)
+def calculate_total_cursing_hours(lead_id: int, db: Session = Depends(get_db)):
+    lead = crud.get_lead(db, lead_id)
+    total_cursing_hours = 0
+    for projection_degree in lead.projection_by_degree:
+        pbd_time = 0
+        for projection_signature in projection_degree.projections_by_signature:
+            projection_signature.pbs_time = projection_signature.signature.time_signature * projection_signature.times
+            pbd_time += projection_signature.pbs_time
+        projection_degree.pbd_time = pbd_time
+        total_cursing_hours += projection_degree.pbd_time
+    lead.total_cursing_hours = total_cursing_hours
+    return lead
