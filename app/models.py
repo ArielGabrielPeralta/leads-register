8226c7cd1f8c98a021cc2fb79189982c99c87ace
ariel <ariel.peralta@eynes.com.ar>
2022-05-01 03:19:00 -0300

    🔗 relationships models

--- app/models.py
+++ app/models.py
@@ -1,13 +1,7 @@
 from sqlalchemy import Column, ForeignKey, Integer, String, Table, Date
 from sqlalchemy.orm import relationship
-from sqlalchemy.orm import validates
 
 from .database import Base
-
-association_table = Table('association', Base.metadata,
-                          Column('bachelors_degrees_id', ForeignKey('bachelors_degrees_id.id'), primary_key=True),
-                          Column('signatures_', ForeignKey('signatures.id'), primary_key=True)
-                          )
 
 
 class Lead(Base):
@@ -16,17 +10,19 @@ class Lead(Base):
     name = Column(String(32), index=True)
     surname = Column(String(32), index=True)
     address = Column(String(110))
-    email = relationship("Email", back_populates="lead", uselist=False)
+    email = Column(String)
     phone = Column(Integer)
     inscription = Column(Date)
-    projections_by_degree = relationship("ProjectionByDegree", back_populates="lead")
+
+    projection_by_degree = relationship("ProjectionByDegree", back_populates='lead')
 
 
 class BachelorsDegree(Base):
     __tablename__ = "bachelors_degrees"
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(48), index=True)
-    signatures = relationship("Signature", back_populates="bachelors_degree", secondary=association_table)
+
+    projections_by_degree = relationship("ProjectionByDegree", back_populates='bachelors_degrees')
 
 
 class Signature(Base):
@@ -34,36 +30,27 @@ class Signature(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(48), index=True)
     duration = Column(Integer, default=4)
-    bachelors_degree_id = Column(Integer, ForeignKey("bachelors_degree.id"))
-    bachelors_degree = relationship("BachelorsDegree", back_populates="signatures", secondary=association_table)
+
+    projections_by_signature = relationship("ProjectionBySignature", back_populates='signature')
 
 
 class ProjectionByDegree(Base):
     __tablename__ = "projections_by_degree"
     id = Column(Integer, primary_key=True, index=True)
-    lead = relationship("Lead", back_populates="projections_by_degree")
-    bachelors_degree = relationship("BachelorsDegree")
-    projections_by_signature = relationship("ProjectionBySignature")
+    lead_id = Column(Integer, ForeignKey('leads.id'))
+    bachelors_degrees_id = Column(Integer, ForeignKey('bachelors_degrees.id'))
+
+    projections_by_signature = relationship("ProjectionBySignature", back_populates='projections_by_degree')
+    bachelors_degrees = relationship("BachelorsDegree", back_populates='projections_by_degree')
+    lead = relationship("Lead", back_populates='projection_by_degree')
 
 
 class ProjectionBySignature(Base):
     __tablename__ = "projections_by_signature"
     id = Column(Integer, primary_key=True, index=True)
-    bachelors_degree = relationship("BachelorsDegree")
-    signature = relationship("Signature")
     times = Column(Integer, default=0)
+    signature_id = Column(Integer, ForeignKey('signatures.id'))
+    projections_by_degree_id = Column(Integer, ForeignKey('projections_by_degree.id'))
 
-
-class Email(Base):
-    __tablename__ = 'emails'
-
-    id = Column(Integer, primary_key=True)
-    email = Column(String)
-    lead_id = Column(Integer, ForeignKey('lead.id'))
-    lead = relationship("Lead", back_populates="email")
-
-    @validates('email')
-    def validate_email(self, key, email):
-        if '@' not in email:
-            raise ValueError("failed simple email validation")
-        return email
+    signature = relationship("Signature", back_populates='projections_by_signature')
+    projections_by_degree = relationship("ProjectionByDegree", back_populates='projections_by_signature')
